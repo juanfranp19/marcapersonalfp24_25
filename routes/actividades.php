374c9c25df2cfbde5869b17e25252c82d4009270
Juanfran Pagán <1913955@alu.murciaeduca.es>
2024-11-26 12:03:51 +0100

    prefix en los archivos de routes

--- routes/actividades.php
+++ routes/actividades.php
@@ -3,10 +3,10 @@
 use App\Http\Controllers\ActividadController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('actividades', [ActividadController::class, 'getIndex']);
+Route::group(['prefix' => 'actividades'], function() {
 
-Route::get('actividades/show/{id}', [ActividadController::class, 'getShow'])->where('id', '[0-9]+');
-
-Route::get('actividades/create', [ActividadController::class, 'getCreate']);
-
-Route::get('actividades/edit/{id}', [ActividadController::class, 'getEdit'])->where('id', '[0-9]+');
+    Route::get('/', [ActividadController::class, 'getIndex']);
+    Route::get('/show/{id}', [ActividadController::class, 'getShow'])->where('id', '[0-9]+');
+    Route::get('/create', [ActividadController::class, 'getCreate']);
+    Route::get('/edit/{id}', [ActividadController::class, 'getEdit'])->where('id', '[0-9]+');
+});
