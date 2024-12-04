fffbf57cf22eb9f0f3efb9982e87b229b7dbabc3
Juanfran Pagán <1913955@alu.murciaeduca.es>
2024-12-04 09:48:42 +0100

    database seeders, factories, query builder

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,21 @@ Route::get('perfil/{id?}', function($id = null) {
 
 
 
-Route::get('pruebaDB/{id?}', function ($votos = null) {
-    $html = '';
+Route::get('pruebaDB/{votos?}', function ($votos = null) {
 
+    $html = getEstadisticas();
+
+    Estudiante::where('nombre', 'Juan')
+        ->where('apellidos', 'Martínez')
+        ->delete();
+
+    $html .= getEstadisticas();
+
+    return $html;
+});
+
+
+function getEstadisticas() {
     $count = Estudiante::where('votos', '>', 100)->count();
     $max = Estudiante::max('votos');
     $min = Estudiante::min('votos');
@@ -37,45 +49,10 @@ Route::get('pruebaDB/{id?}', function ($votos = null) {
     $html .= '<li>Mínimo número de votos: ' . $min . '</li>';
     $html .= '<li>Media de votos: ' . $media . '</li>';
     $html .= '<li>Total de votos: ' . $total . '</li>';
+    $html .= "</ul>";
 
-    $html . "</ul>\n<ul>";
-
-
-    $estudiantes = Estudiante::where('votos', '>', $votos)->take(5)->get();
-
-    foreach( $estudiantes as $estudiante ) {
-        $html .= '<li>' . $estudiante->nombre . '</li>';
-    }
-
-
-
-
-
-
-    $count = Estudiante::where('votos', '>', 100)->count();
-    $html .= 'Antes: ' . $count . '<br />';
-
-    $id = $votos ? $votos : 1;
-
-    $estudiante = Estudiante::findOrFail($id);
-    $estudiante->nombre = 'Juan';
-    $estudiante->apellidos = 'Martínez';
-    $estudiante->direccion = 'Dirección de Juan';
-    $estudiante->votos = 130;
-    $estudiante->confirmado = true;
-    $estudiante->ciclo = 'DAW';
-    $estudiante->save();
-
-    $count = Estudiante::where('votos', '>', 100)->count();
-    $html .= 'Después: ' . $count . '<br />';
-
-
-
-    return $html . '</ul>';
-
-});
-
-
+    return $html;
+}
 
 
 
@@ -84,3 +61,4 @@ include __DIR__.'/curriculos.php';
 include __DIR__.'/proyectos.php';
 include __DIR__.'/reconocimientos.php';
 include __DIR__.'/users.php';
+
