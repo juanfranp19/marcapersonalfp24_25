abc8df22442e0c874eb4588c12375d1b790912da
Juanfran Pagán <1913955@alu.murciaeduca.es>
2024-12-17 08:04:58 +0100

    datos de entrada de proyectos

--- app/Http/Controllers/ProyectosController.php
+++ app/Http/Controllers/ProyectosController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Proyecto;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class ProyectosController extends Controller
@@ -15,9 +16,9 @@ class ProyectosController extends Controller
 
     public function getShow($id)
     {
-            return view('proyectos.show')
-                ->with('proyecto', Proyecto::findOrFail($id))
-                ->with('id', $id);
+        return view('proyectos.show')
+            ->with('proyecto', Proyecto::findOrFail($id))
+            ->with('id', $id);
     }
 
     public function getCreate()
@@ -25,10 +26,44 @@ class ProyectosController extends Controller
         return view('proyectos.create');
     }
 
+    public function store(Request $request): RedirectResponse
+    {
+        $proyecto = new Proyecto;
+        $proyecto->nombre = $request->input('nombre');
+        $proyecto->docente_id = $request->input('docente_id');
+        $proyecto->dominio = $request->input('dominio');
+        // to do: serialize metadatos
+        $proyecto->metadatos = serialize(array($request->input('metadatos')));
+        try {
+            $proyecto->save();
+        } catch (\Exception $e) {
+            return redirect()->action([self::class, 'getCreate'], ['id' => $proyecto->id])
+                ->with('error', $e->getMessage());
+        }
+
+        return redirect()->action([self::class, 'getShow'], ['id' => $proyecto->id]);
+    }
+
     public function getEdit($id)
     {
-            return view('proyectos.edit')
-                ->with('proyecto', Proyecto::findOrFail($id))
-                ->with('id', $id);
+        return view('proyectos.edit')
+            ->with('proyecto', Proyecto::findOrFail($id))
+            ->with('id', $id);
+    }
+
+    public function putEdit(Request $request, $id): RedirectResponse
+    {
+        $proyecto = Proyecto::findOrFail($id);
+        $proyecto->nombre = $request->input('nombre');
+        $proyecto->docente_id = $request->input('docente_id');
+        $proyecto->dominio = $request->input('dominio');
+        try {
+            $proyecto->save();
+        } catch (\Exception $e) {
+            return redirect()->action([self::class, 'getShow'], ['id' => $proyecto->id])
+                ->with('error', $e->getMessage());
+        }
+
+        return redirect()->action([self::class, 'getShow'], ['id' => $proyecto->id]);
     }
 }
--- routes/proyectos.php
+++ routes/proyectos.php
@@ -3,13 +3,17 @@
 use App\Http\Controllers\ProyectosController;
 use Illuminate\Support\Facades\Route;
 
-    Route::group(['prefix' => 'proyectos'], function(){
+Route::group(['prefix' => 'proyectos'], function(){
+
     Route::get('/', [ProyectosController::class, 'getIndex']);
+    Route::post('/', [ProyectosController::class, 'store']);
 
     Route::get('/show/{id}', [ProyectosController::class, 'getShow'])
-    ->where('id', '[0-9]+');
+        ->where('id', '[0-9]+');
 
     Route::get('/create', [ProyectosController::class, 'getCreate']);
 
-    Route::get('/edit/{id}', [ProyectosController::class, 'getEdit'])->where('id', '[0-9]+');
+    Route::get('/edit/{id}', [ProyectosController::class, 'getEdit'])
+        ->where('id', '[0-9]+');
+    Route::put('/{id}', [ProyectosController::class, 'putEdit']);
 });
