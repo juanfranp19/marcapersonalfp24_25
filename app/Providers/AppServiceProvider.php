706a4a489b3cff5069e10cc67fac322270e2fb13
Juanfran Pagán <1913955@alu.murciaeduca.es>
2025-02-13 09:59:48 +0100

    empezando con la autorización por Gates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Curriculo;
+use App\Models\User;
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
     {
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+        });
+
+        Gate::define('update-curriculo', function (User $user, Curriculo $curriculo) {
+            return $user->id === $curriculo->user_id;
         });
     }
 }
