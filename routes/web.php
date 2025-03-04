364635ee7d23741615fb05051c1b357223e885a3
Alberto Sierra <alberto.sierra@murciaeduca.es>
2025-03-04 09:08:10 +0100

    Recuperando las rutas eliminadas por Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+Route::get('/', [HomeController::class, 'getHome']);
+/*
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
@@ -13,6 +16,7 @@ Route::get('/', function () {
         'phpVersion' => PHP_VERSION,
     ]);
 });
+*/
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
@@ -24,4 +28,11 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+include __DIR__.'/actividades.php';
+include __DIR__.'/ciclos.php';
+include __DIR__.'/curriculos.php';
+include __DIR__.'/familias_profesionales.php';
+include __DIR__.'/proyectos.php';
+include __DIR__.'/reconocimientos.php';
+include __DIR__.'/users.php';
 require __DIR__.'/auth.php';
